99feb83a3941cb2deddae8cd8af9288234f9a9be
Jan Van Roy <jan.vanroy@jes.be>
2023-06-02 17:25:39 +0200

    form toelating bis

--- lib/Controller/PageController.php
+++ lib/Controller/PageController.php
@@ -21,12 +21,11 @@ class PageController extends Controller {
 	 */
 	public function index() {
 		Util::addScript(Application::APP_ID, 'jeslinks-main');
-		// Util::addStyle($this->appName, 'icons');
-		$test = new TemplateResponse(Application::APP_ID, 'main');
-		$csp = new ContentSecurityPolicy();
+		Util::addStyle($this->appName, 'icons');
+		return new TemplateResponse(Application::APP_ID, 'main');
+		$csp = new EmptyContentSecurityPolicy();
         $csp->addAllowedFormActionDomain('https://mijnplaats.jes.be');
-		$test->setContentSecurityPolicy($csp);
-		return $test;
 	}
-	
+
+
 }
